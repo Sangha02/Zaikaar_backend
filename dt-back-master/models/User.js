78fd02c7873f8981ce7782f03395de372959aa2d
Sanghamitra Dutta <iradutta02@gmail.com>
2023-05-01 15:01:20 +0530

    First commit

---
+++ dt-back-master/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  gender: String,
+  phone: Number,
+  bookings: [
+    {
+      
+      date: String,
+      time:String,
+      customers: Number,
+    },
+  ],
+});
+
+module.exports = mongoose.model("User", userSchema);
